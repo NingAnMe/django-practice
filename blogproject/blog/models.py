70f36aee19259eb157d92de73ab9035c952ed3db
NingAnMe <ninganme@qq.com>
2017-12-20 11:37:38 +0800

    添加模型

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    '''
+    分类
+    '''
+    name = models.CharField(max_length=30)
+
+
+class Tag(models.Model):
+    '''
+    标签
+    '''
+    name = models.CharField(max_length=30)
+
+
+class Post(models.Model):
+    '''
+    文章
+    '''
+    # 文章标题
+    title = models.CharField(max_length=100)
+
+    # 文章正文
+    body = models.TextField()
+
+    # 文章摘要
+    excerpt = models.CharField(max_length=200, blank=True)
+
+    # 文章分类
+    category = models.ForeignKey(Category)
+
+    # 文章标签
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    # 文章作者
+    author = models.ForeignKey(User)
