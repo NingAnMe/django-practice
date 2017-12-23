2fabc24c07ccb4efa7f6a723daa016f926df272f
NingAnMe <ninganme@qq.com>
2017-12-23 15:07:03 +0800

    详情页视图修改为基于类的通用视图

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
 
     # 文章内容页
-    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
 
     # 文章日期归档页
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$',
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,7 +1,7 @@
 import markdown
 
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from comments.forms import CommentForm
 from .models import Post, Category
 
@@ -15,32 +15,40 @@ class IndexView(ListView):
     context_object_name = 'post_list'
 
 
-def detail(request, pk):
+class PostDetailView(DetailView):
     '''
     文章详情页
     '''
-    post = get_object_or_404(Post, pk=pk)
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
 
-    # 阅读量加 1
-    post.increase_views()
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, *kwargs)
 
-    # Markdown 格式解析
-    post.body = markdown.markdown(post.body,
-                                  extensions=['markdown.extensions.extra',
+        self.object.increase_views()
+
+        return response
+
+    def get_object(self, queryset=None):
+        post = super(PostDetailView, self).get_object(queryset=None)
+        post.body = markdown.markdown(post.body,
+                                      extensions=[
+                                              'markdown.extensions.extra',
                                               'markdown.extensions.codehilite',
-                                              'markdown.extensions.toc', ],
-                                  )
+                                              'markdown.extensions.toc',
+                                          ],)
+        return post
 
-    form = CommentForm()
-    comment_list = post.comment_set.all()
-
-    context = {'post': post,
-               'form': form,
-               'comment_list': comment_list,
-               }
-
-    return render(request, 'blog/detail.html', context=context)
-
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+                'form': form,
+                'comment_list': comment_list,
+            })
+        return context
 
 class ArchivesView(IndexView):
     '''
