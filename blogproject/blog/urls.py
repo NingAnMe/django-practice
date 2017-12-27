e8d8167215f269203544e45895b986e2c972fc54
NingAnMe <ninganme@qq.com>
2017-12-28 00:13:19 +0800

    实现标签归类页面

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
 
     # 文章内容页
-    url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
+    url(r'^post/(?P<pk>[0-9]+)/$',
+        views.PostDetailView.as_view(), name='detail'),
 
     # 文章日期归档页
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$',
@@ -17,4 +18,8 @@ urlpatterns = [
     # 文章分类页
     url(r'^category/(?P<pk>[0-9]+)/$',
         views.CategoryView.as_view(), name='category'),
+
+    # 标签分类页
+    url(r'^tag/(?P<pk>[0-9]+)/$',
+        views.TagView.as_view(), name='tag'),
 ]
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -3,7 +3,7 @@ import markdown
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from comments.forms import CommentForm
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 
 class IndexView(ListView):
@@ -71,3 +71,12 @@ class CategoryView(IndexView):
     def get_queryset(self):
         cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
         return super(CategoryView, self).get_queryset().filter(category=cate)
+
+class TagView(IndexView):
+    '''
+    标签分类页
+    '''
+
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags=tag)
